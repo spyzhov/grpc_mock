e178193bb6b932eb54c4f1dc01177481f5d6cb73
Stepan Pyzhov <spyzhov@newstore.com>
2020-12-12 00:50:13 +0100

    fix build with additional types

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # grpc_mock
 
+Can be used for *.proto files with:
+
+```protobuf
+option go_package = "protob";
+```
+
 Usage:
 
 ```
 docker run -it -v $(pwd)/helloworld/proto:/proto -p 8000:8000 -p 8090:8090 spyzhov/grpc_mock
+```
+
+For additional packages:
+
+```
+docker run -it -v $(pwd)/example/with_types:/proto -e PROTO_PATH=/proto/proto -e PROTO_PATHS="/proto/proto /proto/types" -p 8000:8000 -p 8090:8090 grpc_mock
 ```
 
 Request:
--- build.sh
+++ build.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 set -e
 
-if [[ -z "${PROTO_PATH}" ]]; then
-  echo "env:PROTO_PATH was not set"
-  exit 1
-fi
-
-if [[ -d "${PROTO_PATH}" ]]; then
-  find "${PROTO_PATH}" -name \*.proto -exec cp {} proto/ \;
-else
-  echo "Directory ${PROTO_PATH} doesn't exists"
-  exit 1
-fi
-
-if [[ "$(find proto/ -type f -name '*.proto' | wc -l)" == "0" ]]; then
+if [[ "$(find "${PROTO_PATH}" -type f -name '*.proto' | wc -l)" == "0" ]]; then
   echo "At least one proto file should be set"
   exit 1
 fi
 
 echo "Protoc:Gen"
-protoc --proto_path=proto --go_out=plugins=grpc,paths=source_relative:protob proto/*.proto
+CMD="protoc"
+for path in $PROTO_PATHS; do
+  CMD="${CMD} --proto_path=${path}"
+done
+CMD="${CMD} --go_out=plugins=grpc,paths=source_relative:protob \"${PROTO_PATH}\"/*.proto"
+
+eval "$CMD"
 #protoc --proto_path=proto --descriptor_set_out=service.protoset --include_imports proto/*.proto
 echo "Mock:Gen"
 mock_gen -input="$(pwd)/protob" -output="$(pwd)/mock"
